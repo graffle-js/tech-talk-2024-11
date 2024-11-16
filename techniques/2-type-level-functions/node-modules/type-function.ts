09355336dc90ce07516bcd36c901362f8e613226
Jason Kuhrt <jasonkuhrt@me.com>
2024-11-15 23:27:12 -0500

    minimal example

--- techniques/2-type-level-functions/node-modules/type-function.ts
+++ techniques/2-type-level-functions/node-modules/type-function.ts
@@ -1,3 +1,5 @@
+import { assertType } from '../../lib/assert-equal.js'
+
 export interface TypeFunction {
   // todo mention these must be unknown to work with the `&` (intersection operator) below
   return: unknown
@@ -10,3 +12,25 @@ export namespace TypeFunction {
     $Arguments,
   > = ($TypeFunction & { parameters: $Arguments })['return']
 }
+
+//
+//
+//
+// Minimal Example
+//
+//
+//
+
+interface Plus1 {
+  parameters: string
+  return: `${this['parameters']}+1`
+}
+
+assertType<TypeFunction.Apply<Plus1, '0'>, '0+1'>()
+
+interface MergeA1 {
+  parameters: {}
+  return: this['parameters'] & { a: 1 }
+}
+
+assertType<TypeFunction.Apply<MergeA1, { b: 2 }>, { a: 1; b: 2 }>()
