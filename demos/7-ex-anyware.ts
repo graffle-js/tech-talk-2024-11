574e1ec3ff0a1c12de2475cc94c8fced26a2ab81
Jason Kuhrt <jasonkuhrt@me.com>
2024-11-19 16:52:37 -0500

    tidy

--- demos/7-ex-anyware.ts
+++ demos/7-ex-anyware.ts
@@ -1,14 +1,28 @@
-/**
- * This example shows how you can jump start your anyware into any hook.
- * This is more succinct than having to manually write each hook execution
- * until your reach your desired one.
- */
 import { Graffle } from './assets/graffle/__.js'
 import { url } from './assets/lib/serveSchema.js'
 
 const graffle = Graffle
   .create({ schema: url })
-  // Notice how we **start** with the `exchange` hook, skipping the `encode` and `pack` hooks.
+  //
+  //
+  // -----------------------
+  //
+  //
+  .anyware(async ({ encode }) => {
+    const { pack } = await encode()
+    const { exchange } = await pack()
+    const { unpack } = await exchange({
+      using: {
+        fetch: (request: Request) => {
+          console.log(`E: Intercepted fetch and saw custom header:`, request.headers.get(`x-custom-header`))
+          return new Response(JSON.stringify({ data: { pokemons: [{ name: `Mocked` }] } }))
+        },
+      },
+    })
+    const { decode } = await unpack()
+    const result = await decode()
+    return result
+  })
   //
   //
   // -----------------------
@@ -64,26 +78,6 @@ const graffle = Graffle
         },
       },
     })
-  })
-  //
-  //
-  // -----------------------
-  //
-  //
-  .anyware(async ({ encode }) => {
-    const { pack } = await encode()
-    const { exchange } = await pack()
-    const { unpack } = await exchange({
-      using: {
-        fetch: (request: Request) => {
-          console.log(`E: Intercepted fetch and saw custom header:`, request.headers.get(`x-custom-header`))
-          return new Response(JSON.stringify({ data: { pokemons: [{ name: `Mocked` }] } }))
-        },
-      },
-    })
-    const { decode } = await unpack()
-    const result = await decode()
-    return result
   })
 
 const pokemons = await graffle.query.pokemons({ name: true })
